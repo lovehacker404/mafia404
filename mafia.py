206bdf7174033cbd234246f2b99dc03a01c9c88d
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-06-26 00:23:03 +0500

    Update mafia.py

--- mafia.py
+++ mafia.py
@@ -5,7 +5,7 @@ import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,ge
 os.system('rm -rf .txt')
 for n in range(1000):
 
-    nmbr = random.randint(first_name,last_name,first_name123)
+    nmbr = random.randint(1111111,9999999)
     
     sys.stdout = open('.txt', 'a')
 
